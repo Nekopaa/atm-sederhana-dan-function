42afd7bc28015ebc654462aba93db1d4ca39772f
izan <izansec@gmail.com>
2025-02-05 15:02:12 +0700

    first commit

---
+++ tugas_menghitung luas.py
@@ -0,0 +1,11 @@
+def layang_layang(d1, d2):
+    return 0,5 * d1 * d2
+
+d1 = float(input("Masukkan Nilai diagonal 1 :"))
+d2 = float(input("Masukkan Nilai diagonal 2 :"))
+
+Luas = layang_layang (d1, d2)
+print(f"luas layang layang dengan diagonal1 {d1} dan {d2} adalah {Luas}")
+
+
+
